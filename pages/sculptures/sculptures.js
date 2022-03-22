d03bc92dbe2eecb2da12eaaeaa850da8bfc69382
poldiee <nessidave@gmail.com>
2022-03-22 05:15:20 +0300

    sculptures ui

---
+++ pages/sculptures/sculptures.js
@@ -0,0 +1,28 @@
+$('.nav ul li').click(function(){
+    $(this).addClass("active").siblings.removeClass("active");
+})
+
+
+
+const tabBtn = document.querySelectorAll('.nav ul li');
+const tab = document.querySelectorAll('tab');
+
+function tabs(panelIndex){
+    tab.forEach(function(node){
+        node.style.display = 'none';
+    });
+    tab[panelIndex].style.display = 'block';
+}
+tabs(0);
+
+
+
+
+let bio = document.querySelector('.bio');
+
+function bioText(){
+    bio.oldText = bio.innerText;
+    bio.innerText = bio.innerText.substring(0, 100) + "...";
+    bio.innerHTML += "&nbsp;" + <span onclick='addLength' id='see-more-bio'>See More</span>
+}
+bioText();
