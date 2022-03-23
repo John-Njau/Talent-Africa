7e980ff98765566f120e8b25eb4fa58d67a14809
albright.human <sungutialbright@gmail.com>
2022-03-23 15:35:59 +0300

    add js

---
+++ pages/paintings/paintings.js
@@ -0,0 +1,61 @@
+// User Interface Logic
+$(document).ready(function(){
+    $("#ouma").click(function(){
+        $("#ouma").hide();
+        $("#macharia").hide();
+        $("#ngugi").hide();
+        $("#wamboi").hide();
+        $("#anita").hide();
+        $("#ouma-bio").show();
+        $("#book-ouma").show();
+    
+    })
+    });
+    
+    $(document).ready(function(){
+       $("#macharia").click(function(){
+            $("#ouma").hide();
+            $("#macharia").hide();
+            $("#ngugi").hide();
+            $("#wamboi").hide();
+            $("#anita").hide();
+            $("#macharia-bio").show();
+    
+       })
+    });
+    $(document).ready(function(){
+        $("#ngugi").click(function(){
+            $("#ouma").hide();
+            $("#macharia").hide();
+            $("#ngugi").hide();
+            $("#wamboi").hide();
+            $("#anita").hide();
+            $("#ngugi-bio").show();
+        
+    })
+    });
+    $(document).ready(function(){
+        $("#wamboi").click(function(){
+            $("#ouma").hide();
+            $("#macharia").hide();
+            $("#ngugi").hide();
+            $("#wamboi").hide();
+            $("#anita").hide();
+            $("#lucy-bio").show();
+        
+    })
+    });
+    $(document).ready(function(){
+        $("#anita").click(function(){
+            $("#ouma").hide();
+            $("#macharia").hide();
+            $("#ngugi").hide();
+            $("#wamboi").hide();
+            $("#anita").hide();
+            $("#anita-bio").show();
+        
+    })
+    });
+    
+    
+    
